3712d94eca4cba71c1f1c5eed8401215d3890a9f
yoonjin.jeon <yoonjin.jeon@samsung.com>
2025-08-22 22:35:20 +0900

    init commit

--- app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/geoquiz/MainActivity.kt
@@ -71,6 +71,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
         Log.d(TAG, "onDestroy() called")
+        //test1
     }
     private fun updateQuestion() {
         val questionTextResId = questionBank[currentIndex].textResId
